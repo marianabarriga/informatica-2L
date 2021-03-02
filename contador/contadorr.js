200ee99075f32f7bd72384653180c043d1e75ebd
Liney Castillo Acosta <lineycastilloacosta@MacBook-Pro-de-Liney.local>
2021-03-02 15:27:53 -0500

    contadorr

--- contador/contadorr.js
+++ contador/contadorr.js
@@ -1,14 +1,17 @@
 let contador
 
 if (contador == null){
-    contador == 0
+    contador = 1
     localStorage.setItem ("contador", JSON.stringify(contador))
     console.log("no existe")
+    console.log `ha ingresado ${contador} veces`
+
 } else {
         contador= JSON.parse(localStorage.getItem("contador"))
         contador = contador + 1;
         localStorage.setItem("contador", JSON.stringify(contador))
         console.log ("si existe el contador")
+        console.log `ha ingresado ${contador} veces`
 }
 
 alert(`Se ha ingresado a la página web ${contador} veces`)
